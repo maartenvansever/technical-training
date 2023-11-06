5863dc646f88791041c612fc253a063d57eec97a
maartenvansever <maarten.vansever@citymesh.com>
2023-11-06 10:33:02 +0100

    update: docs

--- estate/models/partner_connect_model.py
+++ estate/models/partner_connect_model.py
@@ -1,5 +1,8 @@
-from odoo import models
+from odoo import models, fields
 
 class PartnerConnectModel(models.Model):
     _name = "connect.partner"
     _description = "This defines the connect partners"
+
+    name = fields.char()
+    split_biller = fields.Boolean(required=True)
