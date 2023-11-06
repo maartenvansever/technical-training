f79fc20a6722e87875c2d6e4277583d44f346975
maartenvansever <maarten.vansever@citymesh.com>
2023-11-06 11:51:26 +0100

    feat: update manifest

--- estate/models/partner_connect_model.py
+++ estate/models/partner_connect_model.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 
 class PartnerConnectModel(models.Model):
     _name = "connect.partner"
-    _description = "This defines the connect partners"
+    _description = "This defines the connect partners."
 
     name = fields.Char()
     split_biller = fields.Boolean()
