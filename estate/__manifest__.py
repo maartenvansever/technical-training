197a734c255eeebd8cbb9f0b0c2031118a623d96
maartenvansever <maarten.vansever@citymesh.com>
2023-11-06 10:53:00 +0100

    feat: add data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,7 @@
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
-
+        'data/connect.partner.csv',
     ],
     "installable": True,
     'license': 'LGPL-3',
