9e7f2f4b86bb489d208323e34175c3cd21b5e536
maartenvansever <maarten.vansever@citymesh.com>
2023-11-06 16:19:24 +0100

    feat: add Many2Many

--- estate/models/partner_connect_model.py
+++ estate/models/partner_connect_model.py
@@ -9,4 +9,4 @@ class PartnerConnectModel(models.Model):
     active = fields.Boolean(default=True)
     phone_number = fields.Char()
     partner_id = fields.Many2one("res.partner", string="Partner")
-    subscription_type = fields.Many2Many("connect.partner.subscription.type", string="Subscription")
+    subscription_type = fields.Many2many("connect.partner.subscription.type", string="Subscription")
