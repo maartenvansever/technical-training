f658833be82b9bad14fac64507a89ea4a0525baa
maartenvansever <maarten.vansever@citymesh.com>
2023-11-07 10:04:52 +0100

    style: shorten table

--- estate/models/product_connect_model.py
+++ estate/models/product_connect_model.py
@@ -1,7 +1,7 @@
 from odoo import models, fields
 
 class ProductConnectModel(models.Model):
-    _name = "connect.partner.subscription.product"
+    _name = "connect.subscription.product"
     _description = "This defines the product fields"
 
     name = fields.Char()
