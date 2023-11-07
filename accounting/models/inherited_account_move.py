c15376fcc27aa3cdcba21a4b2281d1c6fbbd99fa
maartenvansever <maarten.vansever@citymesh.com>
2023-11-07 16:21:57 +0100

    feat: update button

--- accounting/models/inherited_account_move.py
+++ accounting/models/inherited_account_move.py
@@ -4,3 +4,11 @@ class InheritedAccountMove(models.Model):
     _inherit = "account.move"
 
     book_keeper = fields.Char()
+
+    def action_fill_accountant(self):
+        """ When an invoice linked to a sales order selling registrations is
+        paid confirm attendees. Attendees should indeed not be confirmed before
+        full payment. """
+        res = super(InheritedAccountMove, self).action_fill_accountant()
+        self.book_keeper = "some user"
+        return res
