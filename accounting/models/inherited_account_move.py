de0c3a8e9df9bc160da401bb27e2259cf3ca1743
maartenvansever <maarten.vansever@citymesh.com>
2023-11-07 16:45:36 +0100

    feat: update button

--- accounting/models/inherited_account_move.py
+++ accounting/models/inherited_account_move.py
@@ -6,6 +6,5 @@ class InheritedAccountMove(models.Model):
     book_keeper = fields.Char()
 
     def action_fill_book_keeper(self):
-        res = super(InheritedAccountMove, self).action_fill_accountant()
         self.book_keeper = "some user"
-        return res
+        return True
